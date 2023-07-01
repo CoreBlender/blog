d811516d706b92472802879c7968154eeef123bf
CoreBlender <ethan_endures@outlook.com>
2023-07-01 11:12:05 -0400

    Update

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,10 @@
 
 set -e
 curl -fsSL https://bun.sh/install | bash
-export "PATH=$BUN_INSTALL/bin:$PATH"
+export PATH="/opt/buildhome/.bun/bin:$PATH"
 
 bun --version
 bun install
-bun upgrade
 bun run build
 
 
